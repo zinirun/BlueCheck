3f48f254ba04c8d76c44a87045c154e09b935a26
i-msy <simc2644@gmail.com>
2020-05-18 16:55:39 +0900

    회원가입 완료

--- app.js
+++ app.js
@@ -67,74 +67,92 @@ router.route('/defact/drawing/').get(function (req, res) {
 //회원가입 이동 라우터
 router.route('/register').get(function (req, res) {
     fs.readFile('./public/register.html', 'utf8', function (error, data) {
-        res.send(ejs.render(data, {
-        }));
+        res.send(ejs.render(data, {}));
     });
 });
 
+
 //회원가입 라우터
-router.route('/reg_submit').post(function(req,res){
+router.route('/reg_submit').post(function (req, res) {
     var id = req.body.id;
     var pw = req.body.pw;
     var name = req.body.name;
     var tel = req.body.tel;
     var auth = req.body.auth;
     var type = req.body.type;
-    
-    var params = [id,pw,name,tel,type];
-    var result="";
-    console.log('요청 파라미터:'+id+', '+pw+', '+name+', '+tel+', '+auth+', '+type);
-    if(auth==='1234'){
-        checkIfUsed(params,result,res);
-        }
-    
-    else{
-        res.send('<script type="text/javascript">alert("인증코드가 일치하지 않습니다."); window.location="/register";</script>');
-         res.end();
-    }
+    var result;
+    var params = {
+        user_id: id,
+        password: pw,
+        name: name,
+        tel: tel,
+        type: type
+    };
    
+    var alertMsg = "";
+    if (auth === '1234') {
+        result = checkInput(params);
+        if (result == 2) {
+            alertMsg = '연락처 입력이 잘못되었습니다.';
+            res.send('<script type="text/javascript">alert("' + alertMsg + '"); window.location="/register";</script>');
+        } else if (result == 3) {
+            alertMsg = '아이디는 영어/숫자 4-12자리로 입력해주세요.';
+            res.send('<script type="text/javascript">alert("' + alertMsg + '"); window.location="/register";</script>');
+        } else {
+            var checkIdSql = 'SELECT * FROM user WHERE user_id = ?;';
+            mySqlClient.query(checkIdSql, params.user_id, function (err, rows) {
+                if (err) {
+                    console.log("Search Error>>" + err);
+                    alertMsg = "회원등록 중 오류가 발생했습니다.";
+                    res.send('<script type="text/javascript">alert("' + alertMsg + '"); window.location="/register";</script>');
+                } else {
+                    if (rows.length > 0) {
+                        alertMsg = "이미 사용중인 아이디입니다.";
+                        res.send('<script type="text/javascript">alert("' + alertMsg + '"); window.location="/register";</script>');
+                    } else {
+                        var insertSql = 'INSERT INTO user SET ?;';
+                        mySqlClient.query(insertSql, params, function (err) {
+                            if (err) {
+                                console.log("Insert Error>>" + err);
+                                alertMsg = "회원등록 중 오류가 발생했습니다.";
+                                res.send('<script type="text/javascript">alert("' + alertMsg + '"); window.location="/register";</script>');
+                            } else {
+                                alertMsg = "회원가입이 완료되었습니다.";
+                                res.send('<script type="text/javascript">alert("' + alertMsg + '"); window.location="/register";</script>');
+
+                            }
+                        });
+                    }
+
+                }
+            });
+        }
+    } else {
+        res.send('<script type="text/javascript">alert("인증코드가 일치하지 않습니다."); window.location="/register";</script>');
+        res.end();
+    }
 });
 
-function checkIfUsed(params,result,res){
-     mySqlClient.connect();
-     var checkIdSql = 'SELECT * FROM user WHERE user_id = ?;';
-    mySqlClient.query(checkIdSql,params[0],function(err,rows){
-        if(err){
-            console.log("Search Error>>"+err);
-            mySqlClient.end(); return;
+function checkInput(params) {
+    var result;
+    var pattern_eng = /[a-zA-Z]/; //영어 입력 체크 패턴
+    var pattern_num = /[0-9]/; //숫자 입력 체크 
+    var pattern_spc = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
+    var pattern_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; // 한글체크
+    id = params.user_id;
+    tel = params.tel;
+    if ((pattern_num.test(id)) || (pattern_eng.test(id)) && !(pattern_spc.test(id)) && !(pattern_kor.test(id))) { //id검사
+        if ((pattern_num.test(tel)) && !(pattern_eng.test(tel)) && !(pattern_spc.test(tel)) && !(pattern_kor.test(tel))) { //tel검사
+            result = 1; //정상
+        } else {
+            result = 2; // tel 입력 오류
         }
-        else{
-            if(rows.length>0){
-                result = "이미 사용중인 아이디입니다.";
-                printRegisterResult(result,res);
-            }
-            else{
-                addUser(params,result,res);
-            }
-        }
-    });
+    } else {
+        result = 3; //id 입력 오류
+    }
+    return result;
 }
 
-
-function addUser(params,result,res){
-    var insertSql = 'INSERT INTO user (user_id, password, name,tel, type) VALUES (?,?,?,?,?);';
-    mySqlClient.query(insertSql,params,function(err){
-        if(err) 
-            console.log("Insert Error>>"+err); 
-        else {
-            result = "회원가입이 완료되었습니다.";
-            console.log(result);
-            printRegisterResult(result,res);
-        }
-    });
-}
-
-
-function printRegisterResult(result,res){
-    res.send('<script type="text/javascript">alert("'+result+'"); window.location="/register";</script>');
-    mySqlClient.end();
-    res.end();
-}
 
 //-------------------------Mysql------------------------------------
 
