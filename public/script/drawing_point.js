05de2f50aa09903b653ccdd64f833ce30a4e6522
zinirun <heojj97@naver.com>
2020-05-18 16:59:34 +0900

    Update drawing_point.js

--- public/script/drawing_point.js
+++ public/script/drawing_point.js
@@ -1,30 +1,30 @@
 const pixel_point = [
     ["현관", 46, 141],
-    ["거실", 82, 202],
-    ["발코니1", 105, 266],
-    ["발코니2", 58, 42],
+    ["거실", 78, 202],
+    ["발코니1", 101, 266],
+    ["발코니2", 56, 42],
     ["발코니3", 205, 14],
-    ["침실1", 190, 207],
-    ["침실2", 60, 88],
-    ["침실3", 207, 61],
+    ["침실1", 188, 205],
+    ["침실2", 57, 88],
+    ["침실3", 203, 61],
     ["주방/식당", 130, 76],
-    ["욕실1", 184, 130],
-    ["욕실2", 230, 143]
+    ["욕실1", 182, 130],
+    ["욕실2", 225, 143]
 ];
 
 let selected_loc = -1;
 
-function loadDrawingPoint(){
+function loadDrawingPoint() {
     var drawTarget = document.getElementById('div_drawing');
-    
+
     //<div class="px-point"><div class="px-point-title"></div></div>
-    
-    for(var i=0; i<11; i++){
-        var innerCode = '<div class="px-point" id="' + 'point'+i+'" value="'+ pixel_point[i][0]+'"><div class="px-point-title">'+pixel_point[i][0]+'</div></div>'
-        
+
+    for (var i = 0; i < 11; i++) {
+        var innerCode = '<div class="px-point" id="' + 'point' + i + '" value="' + pixel_point[i][0] + '"><div class="px-point-title">' + pixel_point[i][0] + '</div></div>'
+
         drawTarget.innerHTML += innerCode;
-        
-        var pixelTarget = document.getElementById("point"+i);
+
+        var pixelTarget = document.getElementById("point" + i);
         pixelTarget.style.left = pixel_point[i][1] + 'px';
         pixelTarget.style.top = pixel_point[i][2] + 'px';
     }
@@ -35,7 +35,7 @@ function locSelected(user_selected_loc) {
         selected_loc = user_selected_loc.target.getAttribute('value');
         selected_dong = document.getElementById('span_defact_dong').innerHTML;
         selected_ho = document.getElementById('span_defact_ho').innerHTML;
-        
+
         location.href = "/defact/list?dong=" + selected_dong + "&ho=" + selected_ho + "&loc=" + selected_loc;
     }
 }
