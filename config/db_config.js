0e3c04ed66be3eb3fcc1f492f344dee75628cffb
zinirun <heojj97@naver.com>
2020-05-19 21:56:50 +0900

    Fix gitignore for heroku test

---
+++ config/db_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'wjswls1',
+    database: 'bestwaydb',
+    dateStrings: 'date',
+    debug: false
+}
