86a3658eb599b642439477b748bece62036e60fd
zinirun <heojj97@naver.com>
2020-05-24 00:39:18 +0900

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -1,8 +1,8 @@
 module.exports = {
     connectionLimit: 10,
     host: 'localhost',
-    user: 'bestwayuser',
-    password: '1234',
+    user: 'root',
+    password: 'wjswls1',
     database: 'bestwaydb',
     dateStrings: 'date',
     debug: false
