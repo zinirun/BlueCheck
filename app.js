36c8899cfc9911899f767055a3a41fef52b58f7b
zinirun <heojj97@naver.com>
2020-05-24 00:39:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,20 @@ var path = require('path'),
     ejs = require('ejs'),
     fs = require('fs'),
     url = require('url'),
-    cors = require('cors'), //ajax 요청시 cors 지원
-    multer = require('multer') //사진 업로드
+    cors = require('cors'); //ajax 요청시 cors 지원
 
 var mysql = require('mysql');
 const mySqlClient = mysql.createConnection(require('./config/db_config'));
 
+//이미지 업로드 모듈 (POST)
+var imgUpload = require('./routes/img_upload').imgUpload;
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(bodyParser.json());
 app.use('/public', express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.use(expressSession({
@@ -34,12 +36,13 @@ app.use(cors());
 
 var router = express.Router();
 
-
-
 //도면 이동 라우터
 router.route('/defact/drawing/').get(function (req, res) {
     var selected_dong = req.query.dong,
         selected_ho = req.query.ho;
+
+    res.cookie('dong', req.query.dong);
+    res.cookie('ho', req.query.ho);
 
     fs.readFile('./public/view_defact.html', 'utf8', function (error, data) {
         res.send(ejs.render(data, {
@@ -51,9 +54,12 @@ router.route('/defact/drawing/').get(function (req, res) {
 
 //하자 리스트 이동 라우터
 router.route('/defact/list/').get(function (req, res) {
-    var selected_dong = req.query.dong,
-        selected_ho = req.query.ho,
+    var selected_dong = req.cookies.dong,
+        selected_ho = req.cookies.ho,
         selected_loc = req.query.loc;
+
+    res.cookie('loc', req.query.loc);
+
     var selectDefactSql = "select * from defact where dong=? and ho = ? and room=?;";
 
     mySqlClient.query(selectDefactSql, [selected_dong, selected_ho, selected_loc], function (err, rows, fields) {
@@ -89,7 +95,6 @@ router.route('/defact/list/').get(function (req, res) {
 
 //하자 세부 페이지 라우터
 router.route('/defact/detail/').get(function (req, res) {
-
     var defactId = req.query.id;
 
     var selectDetailSql = 'select d.id, d.img, d.construction_name, d.construction_type, d.info, d.due_date, d.is_read, d.is_solved from defact d where d.id = ?';
@@ -153,39 +158,20 @@ router.route('/defact/add/comment/').post(function (req, res) {
     }
 });
 
-//하자 등록 이동 라우터
-router.route('/defact/add/').get(function (req, res) {
-    var selected_dong = req.query.dong,
-        selected_ho = req.query.ho,
-        selected_loc = req.query.loc;
-    console.log(req.query.dong);
-
-    fs.readFile('./public/add_defact.html', 'utf8', function (error, data) {
-        res.send(ejs.render(data, {
-            dong: selected_dong,
-            ho: selected_ho,
-            loc: selected_loc
-        }));
-    });
-});
-
-//--------------------------로그인-------------------------------------
-router.route('/process/login').post();
-
 //공사종류, 동, 호  선택페이지
 router.route('/select').get(function (req, res) {
     if (req.session.user) {
         //동, 호 선택
         if (req.query.ctype) {
-            fs.readFile('./public/select.html','utf8',function(err,data){
+            fs.readFile('./public/select.html', 'utf8', function (err, data) {
                 //선택한 공사종류를 쿠키에 저장
-               res.cookie('ctypeCookie',req.query.ctype);
+                res.cookie('ctype', req.query.ctype);
                 res.send(ejs.render(data, {
                     name: req.session.user.userName,
                     type: req.session.user.userType
                 }));
             });
-        } 
+        }
         //공사종류 선택
         else {
             fs.readFile('./public/select_const.html', 'utf8', function (error, data) {
@@ -202,33 +188,26 @@ router.route('/select').get(function (req, res) {
     }
 });
 
-//하자 등록 라우터
-router.route('/defact/add_submit').post(function (req, res) {
-    var c_name = req.body.construction_name; //공사명
-    var loc = req.body.loc; //dong,ho,room 쪼개야함
-    var c_type = req.body.construction_type; //공종
-    var info = req.body.defact_info; //하자 정보
-    var due_date = req.body.due_date; //기한
-    //이미지 파일 ?
-    var type = req.body.type;
-    var result;
-    var params = {
-        user_id: id,
-        password: pw,
-        name: name,
-        tel: tel,
-        type: type
-    };
 
-});
-
+//메인 페이지 라우터
 var index = require('./routes/index.js');
 router.route('/').get(index);
+
+//회원가입 라우터
 var register = require('./routes/register.js');
 router.route('/register').get(register.register);
 router.route('/reg_submit').post(register.reg_submit);
+
+//로그인 라우터
 var login = require('./routes/login.js');
 router.route('/process/login').post(login);
+
+//하자 등록 라우터 (submit)
+var addDefact = require('./routes/add_defact.js');
+router.route('/defact/add_submit').post(imgUpload.single('img'), addDefact.addDefact);
+
+//하자 등록 이동 라우터
+router.route('/defact/add/').get(addDefact.loadAddDefact);
 
 app.use('/', router);
 
