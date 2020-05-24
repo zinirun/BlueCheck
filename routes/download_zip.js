5212608862e8b0432f3538d53610348534b56afa
zinirun <heojj97@naver.com>
2020-05-24 22:43:14 +0900

    Create download_zip.js

---
+++ routes/download_zip.js
@@ -0,0 +1,27 @@
+var zipper = require('zip-local'),
+    fs = require('fs'),
+    ejs = require('ejs');
+
+function download_redirect(req, res) {
+    createZip(__dirname + '/../public/resources/defact_images/', __dirname + '/../temp/images.zip', function () {
+        console.log("이미지 폴더 압축 완료");
+        const file = `${__dirname}/../temp/images.zip`;
+
+        res.download(file, '전체-하자이미지-압축파일.zip', function (err) {
+            if (err) {
+                res.json({
+                    err: err
+                });
+            } else {
+                res.end();
+            }
+        });
+    });
+}
+
+function createZip(source, out, finish) {
+    zipper.sync.zip(source).compress().save(out);
+    finish();
+}
+
+module.exports.toZip = download_redirect;
