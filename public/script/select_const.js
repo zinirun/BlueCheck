73976cad7916af684906963ea316f17af95a3a42
zinirun <heojj97@naver.com>
2020-05-25 14:35:55 +0900

    Fix Touch Error

--- public/script/select_const.js
+++ public/script/select_const.js
@@ -1,5 +1,8 @@
+const pattern_num = /[0-9]/; //숫자 입력 체크 (클릭 안되게)
+const pattern_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; // 한글체크
+
 document.getElementById('div_ctype_list').addEventListener('click',function(e){
-    if(e.target.innerText.length < 7){
+    if(pattern_kor.test(e.target.innerText) && !pattern_num.test(e.target.innerText)){
         location.href='/select?ctype='+e.target.innerText;
     }
 });
