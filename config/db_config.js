dfb51680caacba450568fa506b4908af7a6c9c88
zinirun <heojj97@naver.com>
2020-05-27 01:11:05 +0900

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -2,10 +2,20 @@ module.exports = {
     connectionLimit: 20,
     host: 'localhost',
     user: 'root',
-    password: 'q1w2e3r4',
-    port: 3306,
+    password: 'wjswls1',
     database: 'bestwaydb',
     dateStrings: 'date',
     debug: false,
     insecureAuth: true
 }
+
+//module.exports = {
+//    connectionLimit: 20,
+//    host: process.env.DATABASE_HOST,
+//    user: 'root',
+//    password: 'q1w2e3r4',
+//    database: 'bestwaydb',
+//    dateStrings: 'date',
+//    debug: false,
+//    insecureAuth: true
+//}
