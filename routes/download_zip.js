7e747c1f67f48836f21affef5b6357f8890416fc
zinirun <heojj97@naver.com>
2020-05-27 17:50:29 +0900

    Update download_zip.js

--- routes/download_zip.js
+++ routes/download_zip.js
@@ -2,22 +2,27 @@ var zipper = require('zip-local'),
     fs = require('fs'),
     ejs = require('ejs');
 
-function download_redirect(req, res) {
-    createZip(__dirname + '/../public/resources/defact_images/', __dirname + '/../temp/images.zip', function () {
-        console.log("이미지 폴더 압축 완료");
-        const file = `${__dirname}/../temp/images.zip`;
+const file = `${__dirname}/../temp/images.zip`;
 
-        res.download(file, '전체-하자이미지-압축파일.zip', function (err) {
-            if (err) {
-                res.json({
-                    err: err
-                });
-                return;
-            } else {
-                res.end();
-                return;
-            }
-        });
+function download_redirect(req, res) {
+    createZip(__dirname + '/../public/resources/defact_images/', __dirname + '/../temp/images.zip', function (err) {
+        if (err) {
+            console.log("압축 중 에러 발생: " + err);
+            res.redirect('/admin/auth');
+        } else {
+            console.log("이미지 폴더 압축 완료");
+            res.download(file, '전체-하자이미지-압축파일.zip', function (err) {
+                if (err) {
+                    console.log("이미지 다운로드 중 에러 발생");
+                    res.redirect('/admin/auth');
+                    return;
+                } else {
+                    res.end();
+                    return;
+                }
+            });
+
+        }
     });
 }
 
