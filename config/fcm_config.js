b3afce2168ab8de0ef99a659d16053c04c8eb47c
so0choi <simc2644@gmail.com>
2020-05-28 02:12:27 +0900

    gitignore update

---
+++ config/fcm_config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdi8giIoP1bZ7pVxwUpELCswJfdXACc-w",
+  authDomain: "bluecheck-37ecc.firebaseapp.com",
+  databaseURL: "https://bluecheck-37ecc.firebaseio.com",
+  projectId: "bluecheck-37ecc",
+  storageBucket: "bluecheck-37ecc.appspot.com",
+  messagingSenderId: "1068061515989",
+  appId: "1:1068061515989:web:568363552787193de3825c",
+  measurementId: "G-Z6HDR9ZSC9"
+};
