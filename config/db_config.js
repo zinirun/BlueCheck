0e67f0e4ec0dd3d62a428aa19ef17fa09227faca
zinirun <heojj97@naver.com>
2020-06-12 16:38:47 +0900

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -1,23 +1,23 @@
-//FOR SERVER
-module.exports = {
-    connectionLimit: 30,
-    host: process.env.DATABASE_HOST,
-    user: 'root',
-    password: '',
-    database: 'bestwaydb',
-    dateStrings: 'date',
-    charset  : 'utf8',
-    debug: false,
-    insecureAuth: true
-}
-//
-////FOR LOCAL TEST
+////FOR SERVER
 //module.exports = {
-//    connectionLimit: 20,
-//    host: 'localhost',
+//    connectionLimit: 30,
+//    host: process.env.DATABASE_HOST,
 //    user: 'root',
-//    password: 'wjswls1',
+//    password: '',
 //    database: 'bestwaydb',
 //    dateStrings: 'date',
-//    debug: false
-//};
+//    charset  : 'utf8',
+//    debug: false,
+//    insecureAuth: true
+//}
+
+//FOR LOCAL TEST
+module.exports = {
+    connectionLimit: 20,
+    host: 'localhost',
+    user: 'root',
+    password: 'wjswls1',
+    database: 'bestwaydb',
+    dateStrings: 'date',
+    debug: false
+};
